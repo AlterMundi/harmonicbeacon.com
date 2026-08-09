a9884d81527561c4b150cc3e1acba9743c72039d
Nicolás Echániz <nicoechaniz@altermundi.net>
2026-08-09 16:19:44 -0300

    feat(site): add Listener hero action

--- tests/listener-link.test.js
+++ tests/listener-link.test.js
@@ -7,6 +7,10 @@ const home = readFileSync(new URL('../index.html', import.meta.url), 'utf8');
 test('the public homepage links visibly to the Listener app in both languages', () => {
   assert.match(
     home,
+    /<a href="https:\/\/listen\.harmonicbeacon\.com\/" class="btn btn-primary">\s*<span data-lang="en">Listen to the Beacon<\/span><span data-lang="es">Escuchar el Beacon<\/span>/,
+  );
+  assert.match(
+    home,
     /<a class="wk-login"[^>]*href="https:\/\/listen\.harmonicbeacon\.com\/">\s*<span data-lang="en">Listen<\/span><span data-lang="es">Escuchar<\/span>/,
   );
 });
